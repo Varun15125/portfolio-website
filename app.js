6977cd395886a923b9c1d57b7be98efbfd109915
unknown <varunlondhe701@gmail.com>
2026-04-09 20:10:49 +0530

    cool features added to your Skills section

---
+++ app.js
@@ -0,0 +1,17 @@
+       function scrollToSection(id) {
+            document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+        }
+
+        function toggleMenu() {
+            document.getElementById('navMenu').classList.toggle('active');
+        }
+
+        const sections = document.querySelectorAll('section');
+        window.addEventListener('scroll', () => {
+            sections.forEach(sec => {
+                const rect = sec.getBoundingClientRect();
+                if (rect.top < window.innerHeight - 100) {
+                    sec.classList.add('show');
+                }
+            });
+        });
