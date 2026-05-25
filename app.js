2c2f12480f71e8408f9100d8c8fa73bf40d2e5f0
Varun15125 <varunlondhe701@gmail.com>
2026-05-25 15:05:22 +0530

    Add Dark mode toggle button

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ function showVisibleSections() {
 
 window.addEventListener('scroll', showVisibleSections);
 window.addEventListener('load', showVisibleSections);
+
+  const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Change button text
+    if (document.body.classList.contains("dark-mode")) {
+      toggleBtn.textContent = "☀️";
+    } else {
+      toggleBtn.textContent = "🌙";
+      
+    }
+  });
